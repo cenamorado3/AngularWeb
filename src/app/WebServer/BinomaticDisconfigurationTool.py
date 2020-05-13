053edd023e689e2e4594fb0ddf90c7458efebc67
Christian Enamorado <cenamorado3@gmail.com>
2020-05-13 16:30:07 -0500

    Binary stuff
    note to self: proceed with caution

--- src/app/WebServer/BinomaticDisconfigurationTool.py
+++ src/app/WebServer/BinomaticDisconfigurationTool.py
@@ -8,19 +8,65 @@ class BinomaticDisconfigurationTool:
                 return False
         return True
 
+    def isChord(self, n):
+        if n % 3 == 0 or n % 5 == 0 or n % 7 == 0:
+            return True
+        else:
+             return False
+
     def ConvertToBinaryArray(self, i):
         binaries = []
-        # for c in bytearray('123', encoding='utf-8'):
-        #     binaries.append(format(c, 'b'))
-        #     print(binaries)
-        # return binaries
         for c in str(i):
+            if not c.isnumeric():
+                raise Exception(c, 'is not numeric')
+                #return {Token: 'Signature verification failed'}
             ibit = format(ord(c), 'b')
-            ibyte = int.to_bytes(int(ibit), 8, 'little')
-            byteString = str(ibyte[2:-1].decode('utf-8'))
-            binaries.append(byteString)
+            binaries.append(self.Pack(ibit))
         return binaries
         
+    def Unpack(self, binArray):
+        bins = []
+        for b in binArray:
+            bins.append(int(b, 2))
+        return self.Flatten(bins)
 
-    def LeftShift(self, b):
-        return b << 1
+    def Flatten(self, bin):
+        flattened = map(str, bin)
+        flattened = ''.join(flattened)
+        return int(flattened)
+        
+
+
+    def Pack(self, b):
+        bits = ''
+        for i in range(len(b)):
+            if i == 0 or i == 1:
+                bits += '0'
+            else:
+                bits += b[i]
+        return bits
+    
+
+    def LeftShift(self, b, n = 1):
+        return b << n
+
+    def RightShift(self, b, n = 1):
+        return b >> 1
+
+    def OrOne(self, b):
+        return b | 1
+
+    def OrZero(self, b):
+        return b | 0
+
+    def Flip(self, b):
+        return ~b
+
+    def AndOne(self, b):
+        return b & 1 
+    
+    def AndZero(self, b):
+        return b & 0
+
+    def Complement(self, b):
+        return (b & -b) + 1
